8efa6b0beb121c661aaced107e327cf8d181a5ab
Treamz <chrnknzhnkv@gmail.com>
2023-12-25 22:12:32 +0100

    new func

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("Hello World");
 const urlParams = new URLSearchParams(window.location.search);
+
+function getBalancers() {
+    return {
+        "rezka": "rezka",
+        "kodik" : "kodik"
+    };
+}
